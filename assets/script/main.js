0f8a43d61a31d85bbaae63f5695564dd8891e289
jonafrankbrimma <jona.frank@brimmatech.com>
2017-12-23 23:39:42 +0530

    Updates

--- assets/script/main.js
+++ assets/script/main.js
@@ -3,5 +3,17 @@ $(document).ready(function() {
 		controlArrows: true,
 		easing: 'easeInOutCubic',
 		scrollingSpeed: 1000,
+		onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){
+			if(direction == 'right') {
+				let className = '.slide-' + Number(slideIndex + 2) + ' ';
+				$( className + '.mid-text').toggleClass('fadeIn');
+				$( className + '.annotation').toggleClass('fadeIn');
+				$( className + '.bigger-text').toggleClass('fadeIn');
+				$( '.ellie-mae-logo' ).toggleClass('fadeIn');
+			}
+		}
 	});
+
+	$('.big-text').textillate({ in: { effect: 'fadeInLeftBig' } });
+	$('.small-text').textillate({ in: { effect: 'fadeInLeftBig' } });
 });
