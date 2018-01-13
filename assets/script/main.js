803affe724e04597f9200cb45ae4e64655d1abf7
jonafrankbrimma <jona.frank@brimmatech.com>
2018-01-13 23:24:19 +0530

    Updated website

--- assets/script/main.js
+++ assets/script/main.js
@@ -3,21 +3,39 @@ $(document).ready(function() {
 		controlArrows: true,
 		easing: 'easeInOutCubic',
 		scrollingSpeed: 1000,
+		scrollOverflow: true,
 		onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){
 			if(direction == 'right') {
 				let className = '.slide-' + Number(slideIndex + 2) + ' ';
 				$( className + '.testimonial-box').toggleClass('fadeIn');
 			}
-		}
+		},
+		onLeave: function(index, nextIndex, direction){
+			if(nextIndex > 1) {
+				$('.header').addClass('header-invert');
+				$('.scroll-btn').addClass('button-invert');
+			} else {
+				$('.header').removeClass('header-invert');
+				$('.scroll-btn').removeClass('button-invert');
+			}
+
+			$('.menu li').removeClass('active-menu');
+			$($('.menu li')[nextIndex - 1]).addClass('active-menu');
+		},
+		anchors:['Home', 'Our-Offerings', 'About-Us']
 	});
 
-	// $('.big-text').textillate({ in: { effect: 'fadeInLeftBig' } });
-	// $('.small-text').textillate({ in: { effect: 'fadeInLeftBig' } });
+	var currentHref = window.location.hash.substr(0);
+
+	$('.menu li').removeClass('active-menu');
+	$("[data-url='"+currentHref+"']").addClass('active-menu');
 });
 
 function menuToggle() {
 	if (isMobile()) {
 		$('.menu-box').toggle( "explode", {}, 500 );
+	} else {
+		$.fn.fullpage.moveTo(1);
 	}
 }
 
