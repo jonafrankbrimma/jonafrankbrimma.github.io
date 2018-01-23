a57ddc9303811a4ae71f195fe07e528e9e9bffe0
jonafrankbrimma <jona.frank@brimmatech.com>
2018-01-23 15:34:58 +0530

    Contact page added

--- assets/script/main.js
+++ assets/script/main.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
 			$('.menu li').removeClass('active-menu');
 			$($('.menu li')[nextIndex - 1]).addClass('active-menu');
 		},
-		anchors:['Home', 'Our-Offerings', 'About-Us', 'Tribal-Leadership']
+		anchors:['Home', 'Our-Offerings', 'About-Us', 'Tribal-Leadership', 'Join-Our-Tribe', 'Contact-Us']
 	});
 
 	var currentHref = window.location.hash.substr(0);
